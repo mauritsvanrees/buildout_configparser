c2c9307fe9da5e8f3c97759d18c3c0d0ab2c8a0d
Maurits van Rees <maurits@vanrees.org>
2020-03-02 19:55:30 +0100

    Initial copy of buildout code.

---
+++ CHANGES.rst
@@ -0,0 +1,8 @@
+Change History
+==============
+
+
+1.0.0 (unreleased)
+------------------
+
+- Initial release.  Code taken from zc.buildout 2.13.
---
+++ setup.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2006-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+long_description = read("README.rst") + "\n" + read("CHANGES.rst")
+
+
+setup(
+    name="buildout_configparser",
+    version="1.0.0.dev0",
+    author="Jim Fulton",
+    author_email="jim@zope.com",
+    description="Config parser taken from buildout",
+    long_description=long_description,
+    license="ZPL 2.1",
+    keywords="configparser buildout",
+    url="http://buildout.org",
+    packages=["buildout_configparser"],
+    package_dir={"": "src"},
+    install_requires=["setuptools",],
+    include_package_data=True,
+    extras_require = dict(
+        test=['manuel']),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
